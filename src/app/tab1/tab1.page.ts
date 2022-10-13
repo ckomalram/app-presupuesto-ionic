e7814ba6fb11d76b01dbffb6aaece8167566b038
ckomalram <glaw14@gmail.com>
2022-10-13 08:22:09 -0500

    agregando alert

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { StorageService } from '../services/storage.service';
+
 
 @Component({
   selector: 'app-tab1',
@@ -8,9 +9,15 @@ import { StorageService } from '../services/storage.service';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-  newPresupuesto: number;
+  handlerMessage = '';
+  roleMessage = '';
 
-  constructor(public storageService: StorageService) {
+  newPresupuesto: number;
+  detalle: string;
+
+  constructor(
+    private alertController: AlertController,
+    public storageService: StorageService) {
     //TODO: Si existe data, presupuesto en localstorage, cargarlo. Sino 0.
     this.newPresupuesto = null;
   }
@@ -21,20 +28,52 @@ export class Tab1Page {
    await this.storageService.savePresupuesto(this.newPresupuesto);
 
      const newDate = new Date();
+     const detalles = this.detalle || 'Otros';
 
     const newData = {
-      desc: 'Quincena',
+      desc: detalles ,
       value: this.newPresupuesto,
       date: newDate,
     };
 
     await this.storageService.saveHistory(newData);
     this.newPresupuesto = null;
+    this.detalle= null;
   }
 
   resetPresupuesto() {
     console.log('Resetear Presupuesto');
     this.newPresupuesto = null;
     this.storageService.remove();
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Reiniciar datos',
+      subHeader: '¿Estás seguro?',
+      message: 'Al seleccionar proceder, se borrarán presupuesto e historial actual',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            this.handlerMessage = 'Alert canceled';
+          },
+        },
+        {
+          text: 'Proceder',
+          role: 'confirm',
+          handler: () => {
+            this.handlerMessage = 'Alert confirmed';
+            this.resetPresupuesto();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    this.roleMessage = `Dismissed with role: ${role}`;
   }
 }
