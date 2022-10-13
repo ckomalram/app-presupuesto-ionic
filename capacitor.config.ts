df29afc4aeadaebeae1d03f5dd5b3a7799e4a786
ckomalram <glaw14@gmail.com>
2022-10-13 01:10:27 -0500

    Tab1+completado

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.komalramtec.presupuesto',
+  appName: 'app-presupuesto',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
