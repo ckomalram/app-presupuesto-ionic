cd3e0e08a4adc4c2bd60668013abefe9308d4fbf
ckomalram <glaw14@gmail.com>
2022-10-12 23:21:50 -0500

    tab 1- completado

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,41 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  presupuesto: number;
+  newPresupuesto: number;
+  dataPresupuesto: any[];
+
+  constructor() {
+
+    //TODO: Si existe data, presupuesto en localstorage, cargarlo. Sino 0.
+    this.presupuesto = 0;
+    this.newPresupuesto= null;
+    this.dataPresupuesto=[];
+  }
+
+
+  agregarPresupuesto(){
+    console.log('Agregando Presupuesto', this.newPresupuesto);
+    this.presupuesto+=this.newPresupuesto;
+
+    const newDate= new Date();
+
+    const newData = {
+      desc: 'Nueva quincena',
+      value: this.newPresupuesto,
+      date: newDate
+    };
+    this.dataPresupuesto.push(newData);
+    this.newPresupuesto = null;
+
+    console.log(this.dataPresupuesto);
+  }
+
+  resetPresupuesto(){
+    console.log('Resetear Presupuesto');
+    this.presupuesto = 0;
+    this.newPresupuesto = null;
+    this.dataPresupuesto=[];
+  }
 
 }
